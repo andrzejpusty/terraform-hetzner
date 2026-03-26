d4825b0711f5230aea294a2d2b99bc957ac6e51c
Andrzej Pusty <andrzej.pusty@gmail.com>
2026-03-26 22:24:47 +0100

    Fix test server type to cx23

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "hcloud_server" "monitoring_vm" {
   ssh_keys    = ["andrzej-vps"]
 }
 
-resource "hcloud_server" "test_vm3" {
+resource "hcloud_server" "test_vm" {
   name        = "devops-test-ci"
   image       = "ubuntu-24.04"
   server_type = "cx23"
